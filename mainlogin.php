00abb31150ff0a18a90c867a89483c058b04a75e
DarkPrince304 <sanchitlucknow@gmail.com>
2015-01-04 19:06:53 +0530

    mainlogin.php
    Main page where user has to login.

---
+++ mainlogin.php
@@ -0,0 +1,31 @@
+
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="stylesheet.css"></link>
+<script type="text/javascript" src="app.js"></script>
+<script src="jquery-1.11.1.min.js" type="text/javascript"></script>
+<script src="app.js" type="text/javascript"></script>
+<script type="text/javascript" src="alphabet.js"></script>
+  </head>
+  <body>
+    <canvas id="myCanvas"></canvas>
+    <script type="text/javascript" src="bubbles.js"></script>
+    <script type="text/javascript" src="main.js"></script>
+</head>
+<title>The Stereo Jam!</title>
+<body>
+
+<div class="Form">
+<form action="checklogin.php" method="post">
+Username: <input type="text" name="Id">
+<br>
+Password: <input type="password" name="Password">
+<br>
+<input type="Submit">
+</form>
+<p>New User? <a href="page2.php">Register Here</a></p>
+</div>
+
+
+</body>
+</html>
